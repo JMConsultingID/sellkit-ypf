f6d8fe8af60b7604ad4d45944e132903d8a8e505
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-03-12 11:29:31 +0800

    Update sellkit-ypf.php

--- sellkit-ypf.php
+++ sellkit-ypf.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Sellkit Add-on - YPF Checkout
  * Plugin URI:        https://yourpropfirm.com/
  * Description:       Plugin for Customize Sellkit Checkout Page
- * Version:           1.0.29
+ * Version:           1.0.30
  * Author:            Ardi
  * Author URI:        https://yourpropfirm.com/
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.4 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'SELLKIT_YPF_VERSION', '1.0.29' );
+define( 'SELLKIT_YPF_VERSION', '1.0.30' );
 
 /**
  * The code that runs during plugin activation.
